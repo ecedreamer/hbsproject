74e4cae74322b50fd39ccf05bad9466a5032b01c
ecedreamer <sangit.niroula@gmail.com>
2021-04-23 08:50:32 +0545

    admin login link in client side

--- hbsproject/settings.py
+++ hbsproject/settings.py
@@ -146,5 +146,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "noreply.necommerce@gmail.com"
-EMAIL_HOST_PASSWORD = "ece@9886D"
+EMAIL_HOST_USER = "youremail@gmail.com"
+EMAIL_HOST_PASSWORD = "yourpassword"
